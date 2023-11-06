dcf502c0fb04f126cde0566f25cf464f19b04b3e
Tomer Tal <tomertal@Tomers-MBP.hitronhub.home>
2023-11-06 12:32:23 -0500

    removed POSTGRES_URL

--- server/models/RequestModel.js
+++ server/models/RequestModel.js
@@ -2,7 +2,11 @@ const { Pool } = require('pg');
 
 //create a new sqlite3 database
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });;
 
 //create the request table if it doesn't exist
--- server/server.js
+++ server/server.js
@@ -15,8 +15,11 @@ const PORT = process.env.PORT || 3000;
 
 // Connect server to the PostgreSQL database
     const pool = new Pool({
-        connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
     });
 
     pool.connect()
