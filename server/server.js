e0a60cd82c0a6c71ed401a58ff24553f74a5c02d
Tomer Tal <tomertal@Tomers-MBP.hitronhub.home>
2023-11-06 12:16:44 -0500

    added vercel postgres

--- server/server.js
+++ server/server.js
@@ -15,11 +15,8 @@ const PORT = process.env.PORT || 3000;
 
 // Connect server to the PostgreSQL database
     const pool = new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
+        connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+
     });
 
     pool.connect()
